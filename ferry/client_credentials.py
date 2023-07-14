15166473c8ec73571af3251b80c7b5f21bca12c7
yacquub <97082343+bblankfield@users.noreply.github.com>
2023-07-14 19:26:17 -0400

    Finish integration -- fix import error

---
+++ ferry/client_credentials.py
@@ -0,0 +1,7 @@
+# CLIENT_ID & CLIENT_SECRET set in environment variables (held in .zshrc)
+def get_client_id() -> str:
+    return CLIENT_ID
+
+
+def get_client_secret() -> str:
+    return CLIENT_SECRET
